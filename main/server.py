a96b7621387343c621d5c88f66e09f46c4a3fe78
jduhking <112415714+jduhking@users.noreply.github.com>
2022-11-29 22:32:29 -0600

    Finished with a few minor bugs

--- main/server.py
+++ main/server.py
@@ -4,8 +4,8 @@ server_host = '10.0.0.1'
 server_port = 5000
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((server_host, server_port))
-    s.listen()
+    s.bind(('', server_port))
+    s.listen(2)  # listen to two hosts at a time, renderer and controller
     conn, addr = s.accept()
     with conn:
         print("Connected to addr={0} \n Listening on port x={1}".format(
