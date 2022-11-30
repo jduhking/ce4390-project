31ee8a95a21812ae81797a3ea33542e544e7e834
jduhking <112415714+jduhking@users.noreply.github.com>
2022-11-29 20:37:23 -0600

    Added renderer

--- main/rcst_protocol_util.py
+++ main/rcst_protocol_util.py
@@ -1,3 +1,39 @@
-# Protocol utilities.
+# This file holds all the necessary functions needed to carry out our protocol
 
 import json
+
+
+class messageTypes:
+
+    RETRIEVE = 0
+    RENDER = 1
+    STREAM = 2
+    CLOSE = 3
+    RESPONSE = 4
+
+
+# function to convert the data into bytes
+
+
+def convertToBytes(payload, ignoreDictionary=False):
+
+    # This function converts our data into bytes to sent over network
+
+    if isinstance(payload, str):  # return a single bytified value
+        return payload.encode('utf-8')
+
+    if isinstance(payload, list):  # if it is a list of values return a list of bytified value
+        return [convertToBytes(data, ignoreDictionary=True) for data in payload]
+    # if the data is a dictionary return a dictionary of bytified key and value pairs
+
+    if isinstance(payload, dict) and not ignoreDictionary:
+
+        return {
+
+            convertToBytes(key, ignoreDictionary=True): convertToBytes(value, ignoreDictionary)
+            for key, value in payload.items()
+        }
+
+    # spit the unchanged data back if it is not applicable
+
+    return payload
