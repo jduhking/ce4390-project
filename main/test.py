ac8e373269d58177caae6d7cb6fcb07f8acb8ab4
jduhking <112415714+jduhking@users.noreply.github.com>
2022-11-29 18:09:08 -0600

    edited test.py

--- main/test.py
+++ main/test.py
@@ -1,10 +1,1 @@
-from mininet.net import Mininet
-from mininet.topo import LinearTopo
-
-Linear = LinearTopo(k=3)
-
-net = Mininet(topo=Linear)
-
-net.start()
-net.pingAll()
-net.stop()
+print("Hello")
