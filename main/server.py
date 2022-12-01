c1c8259be432334aed7606f5b0ccca028cd45e65
jduhking <112415714+jduhking@users.noreply.github.com>
2022-11-30 19:54:38 -0600

    Fixed server.py

--- main/server.py
+++ main/server.py
@@ -29,107 +29,109 @@ server_port = 5000
 
 def startListening():
 
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as serverSocket:
-        # Avoid bind() exception: OSError: [Errno 48] Address already in use
-        serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # Avoid bind() exception: OSError: [Errno 48] Address already in use
+    serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-        # listen to two hosts at a time, renderer and controller
+    print("Socket initialized")
 
-        # Try to bind
+    # listen to two hosts at a time, renderer and controller
+
+    # Try to bind
+
+    try:
+        serverSocket.bind(
+            (NodeAddresses.serverIP, NodePorts.serverPort))
+        print("Socket bind is successful..")
+    except socket.error as msg:
+
+        print("Bind was uncessful. Error: " + str(msg))
+        sys.exit()
+
+    # After the bind is sucessful, start listening
+
+    serverSocket.listen(2)
+
+    print("Server listening on port " + str(NodePorts.controllerPort))
+
+    # Turn the server on
+
+    serverIsOn = True
+
+    while serverIsOn:
+
+        # Establish a connection
+
+        conn, addr = serverSocket.accept()
+
+        print("Connected to {0} \nListening on port {1}".format(
+            str(addr), str(NodePorts.serverPort)))
 
         try:
-            serverSocket.bind(
-                (NodeAddresses.serverIP, NodePorts.serverPort))
-            print("Socket bind is successful..")
-        except socket.error as msg:
 
-            print("Bind was uncessful. Error: " + str(msg))
-            sys.exit()
+            # receive message from the socket
 
-        # After the bind is sucessful, start listening
+            message = conn.recv(1024)
 
-        serverSocket.listen(2)
+            # There are three types of requests, RETRIEVE, RENDER AND STREAM
+            # For request messages
+            # Get the RCST request type, get the fileName and all useful information from request line
+            # { "MessageType": < val >, "ResourceName": < val >, "ServerIp" : <val>, "RCSTVersion": <val> }
+            # For each request there is a corresponding response
+            # For controller to server when initiating RETRIEVE, the response from the server is a response line and a payload
+            # decode the message
 
-        print("Server listening on port " + str(NodePorts.controllerPort))
+            message = message.decode('utf-8')
 
-        # Turn the server on
+            message = json.loads(message)
 
-        serverIsOn = True
+            print(message)
 
-        while serverIsOn:
+            # capture request type
 
-            # Establish a connection
+            messageType = message['MessageType']
 
-            conn, addr = serverSocket.accept()
+            print(messageType)
 
-            with conn:
-                print("Connected to {0} \nListening on port {1}".format(
-                    str(addr), str(NodePorts.serverPort)))
+            # capture the fileName
 
-                try:
+            resourceName = message['ResourceName']
 
-                    # receive message from the socket
+            print(resourceName)
 
-                    message = conn.recv(1024)
-                    print("message is " + str(message))
+            if messageType == MessageType.RETRIEVE:
 
-                    # There are three types of requests, RETRIEVE, RENDER AND STREAM
-                    # For request messages
-                    # Get the RCST request type, get the fileName and all useful information from request line
-                    # { "MessageType": < val >, "ResourceName": < val >, "ServerIp" : <val>, "RCSTVersion": <val> }
-                    # For each request there is a corresponding response
-                    # For controller to server when initiating RETRIEVE, the response from the server is a response line and a payload
-                    # decode the message
+                print("Retrieve a list")
 
-                    message = message.decode('utf-8')
+            elif messageType == MessageType.RENDER:
 
-                    message = json.loads(message)
+                print("Render a media file")
 
-                    # capture request type
+            elif messageType == MessageType.STREAM:
 
-                    messageType = message['MessageType']
+                print("Stream a media file")
 
-                    print(messageType)
+            elif messageType == MessageType.RESPONSE:
 
-                    # capture the fileName
+                print("This is the response of an RCST request message")
 
-                    resourceName = message['ResourceName']
+            elif messageType == MessageType.CLOSE:
 
-                    print(resourceName)
+                print("Server is shutting down")
+                serverIsOn = False
 
-                    if messageType == MessageType.RETRIEVE:
+        except IOError as err:
 
-                        print("Retrieve a list")
+            # If an error is present send the message to the controller and close the socket
 
-                    elif messageType == MessageType.RENDER:
+            print("IOError: " + str(err))
 
-                        print("Render a media file")
+        # Close connection socket after handling message
+        conn.close()
 
-                    elif messageType == MessageType.STREAM:
+    # close server socket
 
-                        print("Stream a media file")
-
-                    elif messageType == MessageType.RESPONSE:
-
-                        print("This is the response of an RCST request message")
-
-                    elif messageType == MessageType.CLOSE:
-
-                        print("Server is shutting down")
-                        serverIsOn = False
-
-                except IOError as err:
-
-                    # If an error is present send the message to the controller and close the socket
-
-                    print("IOError: " + str(err))
-
-             # Close connection socket after handling message
-            conn.close()
-
-        # close server socket
-
-        serverSocket.close()
+    serverSocket.close()
 
 
 startListening()
