16be0f5a31c9db599eacce5617e48f98a2c338cb
jduhking <112415714+jduhking@users.noreply.github.com>
2022-11-30 19:26:13 -0600

    updated controller protocol

--- main/controller.py
+++ main/controller.py
@@ -18,7 +18,7 @@ from rcst_protocol_util import *
 
 # important variables
 
-server_host = '10.0.0.1'  # IP of the server
+server_host = '127.0.0.1'  # IP of the server
 server_port = 5000  # server port
 
 # controller functions
@@ -30,18 +30,59 @@ def getInput():  # get user input and send a request based on that input
                    "1) Get list of media files from the server"
                    "2) Open the file. \n"
                    "3) Close. \n")
-    '''
-        while True:  # block until user has entered a value
-    try:  # enter the value
-        userInput = int()
-    '''
+
+    while True:  # block until user has entered a value
+        try:  # enter the value
+            userInput = int(
+                input(menuOptions + "\n" + "Please select an option"))
+        except ValueError:
+            print("Wrong Input, try again")
+            continue  # continually prompt for input until correct value is entered
+    # Make sure if the input is within the range
+
+        if userInput < 1 or userInput > 3:
+            print("Invalid option, try again")
+        else:  # Once the correct input is selected, then break out of validation loop
+            break
+    return userInput
+
+# Send a request to the server to retreive a list of media files located on the server
 
 
-# wait for user input and send requests based on that
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((server_host, server_port))
+def RetrieveFiles():
+    # Create RCST request message for getting the list of files
 
-    s.sendall(b'Hello!, world')
-    data = s.recv(256)
+    request = json.dumps({"MessageType": MessageType.RETRIEVE,
+                         "ResourceName": "mediaFiles", "ServerIP": "127.0.0.1", "RCSTVersion": "1.0"})
+    request = request.encode('utf-8')
+    # Create socket
 
-print('Received', repr(data))
+    controllerSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # Connect to server socket
+
+    controllerSocket.connect((NodeAddresses.serverIP, NodePorts.serverPort))
+
+    # send request to server
+
+    controllerSocket.sendall(request)
+
+# close the socket
+
+    controllerSocket.close()
+
+# Run the controller functions
+
+# get the input and then perform a function depending on the value
+
+
+controllerInput = int
+
+while controllerInput != 3:
+    controllerInput = getInput()
+
+    if controllerInput == 1:  # Get the list of media files
+        RetrieveFiles()
+
+    elif controllerInput == 3:
+        sys.exit()
--- main/server.py
+++ main/server.py
@@ -1,25 +1,134 @@
+'''
+This is the server class, it listens for requests
+from the controller and receiver
+
+For the controller, it returns a list of files
+For the renderer it streams the files to it
+
+'''
+# import dependencies
+
 import socket
+import json
+import os
+from os import listdir
+from os.path import isfile, join
+import sys
+import rcst_protocol_util
+from rcst_protocol_util import *
 
-server_host = '10.0.0.1'
+# get the path of the folder holding the media files
+
+mediaFolder = os.path.normpath("files")
+server_host = '127.0.0.1'
 server_port = 5000
+# server functions
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind(('', server_port))
-    s.listen(2)  # listen to two hosts at a time, renderer and controller
-    conn, addr = s.accept()
-    with conn:
-        print("Connected to addr={0} \n Listening on port x={1}".format(
-            addr, server_port))
-        while True:
+# this function starts the server and listens for requests
 
-            data = conn.recv(1024)
-            if data:
-                print("Data received is:", repr(data))
-                # convert the bytes data object to string (utf-8)
-                data = data.decode("utf-8")
-                data = data.upper()  # upper case the data
-                print("Uppercasing the data gives: " + data)
-                data = data.encode()
-            if not data:
-                break
-            conn.sendall(data)
+
+def startListening():
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as serverSocket:
+        # Avoid bind() exception: OSError: [Errno 48] Address already in use
+        serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+        # listen to two hosts at a time, renderer and controller
+
+        # Try to bind
+
+        try:
+            serverSocket.bind((server_host, server_port))
+            print("Socket bind is successful..")
+        except socket.error as msg:
+
+            print("Bind was uncessful. Error: " + str(msg))
+            sys.exit()
+
+        # After the bind is sucessful, start listening
+
+        serverSocket.listen(2)
+
+        print("Server listening on port " + str(server_port))
+
+        # Turn the server on
+
+        serverIsOn = True
+
+        while serverIsOn:
+
+            # Establish a connection
+
+            conn, addr = serverSocket.accept()
+
+            with conn:
+                print("Connected to {0} \nListening on port {1}".format(
+                    str(addr), str(server_port)))
+
+                try:
+
+                    # receive message from the socket
+
+                    message = conn.recv(1024)
+                    print("message is " + str(message))
+
+                    # There are three types of requests, RETRIEVE, RENDER AND STREAM
+                    # For request messages
+                    # Get the RCST request type, get the fileName and all useful information from request line
+                    # { "MessageType": < val >, "ResourceName": < val >, "ServerIp" : <val>, "RCSTVersion": <val> }
+                    # For each request there is a corresponding response
+                    # For controller to server when initiating RETRIEVE, the response from the server is a response line and a payload
+                    # decode the message
+
+                    message = message.decode('utf-8')
+
+                    message = json.loads(message)
+
+                    # capture request type
+
+                    messageType = message['MessageType']
+
+                    print(messageType)
+
+                    # capture the fileName
+
+                    resourceName = message['ResourceName']
+
+                    print(resourceName)
+
+                    if messageType == MessageType.RETRIEVE:
+
+                        print("Retrieve a list")
+
+                    elif messageType == MessageType.RENDER:
+
+                        print("Render a media file")
+
+                    elif messageType == MessageType.STREAM:
+
+                        print("Stream a media file")
+
+                    elif messageType == MessageType.RESPONSE:
+
+                        print("This is the response of an RCST request message")
+
+                    elif messageType == MessageType.CLOSE:
+
+                        print("Server is shutting down")
+                        serverIsOn = False
+
+                except IOError as err:
+
+                    # If an error is present send the message to the controller and close the socket
+
+                    print("IOError: " + str(err))
+
+             # Close connection socket after handling message
+            conn.close()
+
+        # close server socket
+
+        serverSocket.close()
+
+
+startListening()
